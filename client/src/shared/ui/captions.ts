fc72d545c1e3fcbb0c48ce76c1836fd4f65d909b
Schcerbakov Maxim <miko.r4r@gmail.com>
2026-01-06 16:39:32 +0300

    feat: add title and body

---
+++ client/src/shared/ui/captions.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const TitleXS = styled.p`
+  color: #000;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 30px;
+  font-family: Nunito;
+`;
+
+export const BodyM = styled.p`
+  color: #000;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 28px;
+  font-family: Nunito;
+`;
