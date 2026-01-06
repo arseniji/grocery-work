93f17de94e4cd9374f2e9ac3059dc6ff6fb423db
Schcerbakov Maxim <miko.r4r@gmail.com>
2026-01-06 17:48:49 +0300

    feat: header sticky

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,24 +1,16 @@
 import { HomePage } from "@/pages/home-page";
 import { Header } from "@/widgets";
 import { BrowserRouter, Route, Routes } from "react-router";
-import styled from "styled-components";
 
 function App() {
   return (
-    <Container>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
-    </Container>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-const Container = styled.div`
-  max-width: 1440px;
-  margin: 0 auto;
-`;
 
 export default App;
