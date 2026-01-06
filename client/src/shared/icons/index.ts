e75d21ffb3c723862dc25653eb3a9d1245d036b1
Schcerbakov Maxim <miko.r4r@gmail.com>
2026-01-06 18:42:51 +0300

    feat: add adj icons

--- client/src/shared/icons/index.ts
+++ client/src/shared/icons/index.ts
@@ -1,3 +1,7 @@
 export { LogoIcon } from "./logo-icon";
 export { SearchIcon } from "./search-icon";
 export { CartIcon } from "./cart-icon";
+export { EarpodsIcon } from "./earpods-icon";
+export { ShieldIcon } from "./shield-icon";
+export { RefreshIcon } from "./refresh-icon";
+export { BoxIcon } from "./box-icon";
