3f7d37671e896ba45e2947d25b286d68f896a318
Schcerbakov Maxim <miko.r4r@gmail.com>
2026-01-06 16:43:39 +0300

    feat: add nav

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,5 +1,23 @@
+import { Header } from "@/widgets";
+import { BrowserRouter, Route, Routes } from "react-router";
+import styled from "styled-components";
+
 function App() {
-  return <></>;
+  return (
+    <Container>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<></>} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  max-width: 1440px;
+  margin: 0 auto;
+`;
 
 export default App;
