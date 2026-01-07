03aec00b1f626c51e794186d86d4830c993c9c07
Schcerbakov Maxim <miko.r4r@gmail.com>
2026-01-07 15:06:42 +0300

    feat: add regexp and transform

--- client/src/lib/validators/base-schemas/string-schema.ts
+++ client/src/lib/validators/base-schemas/string-schema.ts
@@ -1,11 +1,16 @@
 import type { BaseValidatorOptions, Rule, ValidatorError } from "../types";
 import { Schema } from "./schema";
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+interface StringValidatorOptions extends BaseValidatorOptions {}
+
 export class StringSchema extends Schema<string> {
   private rules: Rule[] = [];
+  private schemaValue: unknown;
 
-  constructor(options?: BaseValidatorOptions) {
+  constructor(options?: StringValidatorOptions) {
     super();
+    this.schemaValue = undefined;
     this.rules.push({
       message: options?.message || "Поле должно быть строкой",
       ruleFunction: (value: unknown) => typeof value === "string",
@@ -13,15 +18,16 @@ export class StringSchema extends Schema<string> {
   }
 
   validate(value: unknown): string | ValidatorError {
+    this.schemaValue = value;
     for (const rule of this.rules) {
-      if (!rule.ruleFunction(value)) {
+      if (!rule.ruleFunction(this.schemaValue)) {
         return { message: rule.message };
       }
     }
     return value as string;
   }
 
-  min(length: number, options?: BaseValidatorOptions): StringSchema {
+  min(length: number, options?: StringValidatorOptions): StringSchema {
     const msg = options?.message || `Минимальная длина поля ${length}`;
     this.rules.push({
       message: msg,
@@ -31,13 +37,48 @@ export class StringSchema extends Schema<string> {
     return this;
   }
 
-  max(length: number, options?: BaseValidatorOptions): StringSchema {
+  max(length: number, options?: StringValidatorOptions): StringSchema {
     const msg = options?.message || `Максимальная длина поля ${length}`;
     this.rules.push({
       message: msg,
       ruleFunction: (value: string) => value.length <= length,
     });
 
+    return this;
+  }
+
+  regexp(reg: RegExp, options?: StringValidatorOptions): StringSchema {
+    const msg =
+      options?.message || `Поле не выполняет требования регклярного выражения`;
+
+    this.rules.push({
+      message: msg,
+      ruleFunction: (value: string) => reg.test(value),
+    });
+
+    return this;
+  }
+
+  transform(
+    transformator: (value: string) => string,
+    options?: StringValidatorOptions
+  ): StringSchema {
+    const msg = options?.message || `Поле нельзя преобразовать`;
+    try {
+      this.schemaValue = transformator(this.schemaValue as string);
+
+      this.rules.push({
+        message: msg,
+        ruleFunction: () => true,
+      });
+    } catch (err) {
+      const error = err as Error;
+
+      this.rules.push({
+        message: error.message,
+        ruleFunction: () => false,
+      });
+    }
     return this;
   }
 }
