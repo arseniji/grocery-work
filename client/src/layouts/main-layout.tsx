fd24f33330f1d58d92ef5f01d6bc3ecdd840ff3a
Schcerbakov Maxim <miko.r4r@gmail.com>
2026-01-08 18:12:02 +0300

    feat: add loader

--- client/src/layouts/main-layout.tsx
+++ client/src/layouts/main-layout.tsx
@@ -1,11 +1,12 @@
 import { useAuth } from "@/lib/hooks";
 import { Footer, Header } from "@/widgets";
+import { Loader } from "@/shared/ui";
 import { Outlet } from "react-router";
 
 export const MainLayout = () => {
   const { isAuth, isLoading } = useAuth();
 
-  if (isLoading) return;
+  if (isLoading) return <Loader />;
 
   return (
     <>
