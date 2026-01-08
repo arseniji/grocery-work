8fb2998a6e1502f9521575b4bac95200d8e1a880
KotDev <kotikov.lil@bk.ru>
2026-01-08 18:47:42 +0300

    добавил контроллер корзины

---
+++ server/app/controllers/api/v1/cart_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::CartController < BaseController
+  
+  def add_product_to_cart
+    product_id = params[:product_id]
+    quantity = params[:quantity]
+    result = CartManager.add_product_to_cart(product_id, quantity, @current_session_id)
+    render json: result
+  end
+
+  def delete_product_on_cart
+    product_id = params[:product_id]
+    result = CartManager.delete_product_on_cart(product_id, @current_session_id)
+    render json: result
+  end
+
+  def delete_quantity_product_on_cart
+    product_id = params[:product_id]
+    result = CartManager.delete_quantity_product_on_cart(product_id, @current_session_id)
+    render json: result
+  end
+
+  def clear_products_to_cart
+    result = CartManager.clear_products_to_cart(@current_session_id)
+    render json: result
+  end
+end
