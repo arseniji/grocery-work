4145789217dbe0e39ac21bee44fdae7e8e23f822
Schcerbakov Maxim <miko.r4r@gmail.com>
2026-01-10 17:10:13 +0300

    feat: add ui

--- client/src/pages/product-page/ui/index.tsx
+++ client/src/pages/product-page/ui/index.tsx
@@ -4,6 +4,20 @@ import { productsApi } from "@/lib/api/products";
 import type { AxiosError } from "axios";
 import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router";
+import {
+  Main,
+  ProductContainer,
+  Image,
+  Details,
+  Category,
+  Price,
+  Description,
+  Unit,
+  Quantity,
+} from "./styled";
+import { TitleL, TitleM } from "@/shared/ui/captions";
+import { StarRating } from "@/shared/ui/star-rating";
+import { Button, Loader } from "@/shared/ui";
 
 export const ProductPage = () => {
   const { id } = useParams();
@@ -19,17 +33,17 @@ export const ProductPage = () => {
 
       if (response.success) {
         setProduct(response);
-        setIsLoading(false);
-        return;
       }
     } catch (err) {
       const error = err as AxiosError;
       Toast.show({
         title: "Ошибка",
-        msg: (error.response?.error.message as string) || "Неизетсная ошибка",
+        msg: "Неизвестная ошибка",
         type: "error",
       });
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   }, [id]);
 
@@ -37,5 +51,41 @@ export const ProductPage = () => {
     loadProduct();
   }, [loadProduct]);
 
-  return <></>;
+  if (isLoading) {
+    return (
+      <Main>
+        <Loader />
+      </Main>
+    );
+  }
+
+  if (!product) {
+    return (
+      <Main>
+        <TitleM>Продукт не найден</TitleM>
+      </Main>
+    );
+  }
+
+  return (
+    <Main>
+      <ProductContainer>
+        <Image src={product.details.image_url} alt={product.name} />
+        <Details>
+          <Category>{product.category}</Category>
+          <TitleL>{product.name}</TitleL>
+          <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+            <Price>{product.price}р</Price>
+            <StarRating rating={parseFloat(product.rating)} />
+          </div>
+          <Description>{product.details.description}</Description>
+          <div style={{ display: "flex", gap: "20px" }}>
+            <Unit>Единица: {product.details.unit}</Unit>
+            <Quantity>Количество: {product.details.quantity}</Quantity>
+          </div>
+          <Button variant="primary">Добавить в корзину</Button>
+        </Details>
+      </ProductContainer>
+    </Main>
+  );
 };
