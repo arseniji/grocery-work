c159f1327b8b0cf5acf13687c6a843a5d29b270a
Schcerbakov Maxim <miko.r4r@gmail.com>
2026-01-10 16:54:40 +0300

    fix: imports

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import { RegistrationPage } from "@/pages/registration-page";
 import { ToastProvider } from "@/feat/toast";
 import { ShopPage } from "@/pages/shop-page";
-import { ProductPage } from "@/pages/product-page/ui";
+import { ProductPage } from "@/pages/product-page";
 
 function App() {
   return (
