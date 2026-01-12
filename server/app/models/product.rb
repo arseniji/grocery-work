b780a59a494e6c8a2d8fe9741716c5eee52d4dda
SilerHofe <sashsa220605@mail.ru>
2026-01-12 19:18:09 +0300

    добавил мини-валидацию на product

--- server/app/models/product.rb
+++ server/app/models/product.rb
@@ -2,6 +2,40 @@ class Product < ApplicationRecord
   has_many :order_items, dependent: :destroy
   has_many :orders, through: :order_items
   
-  validates :product_name, :price, :measurement_unit, presence: true
-  validates :price, numericality: { greater_than: 0 }
+  #Название продукта
+  validates :product_name, 
+    presence: { message: "не может быть пустым" },
+    length: { 
+      minimum: 2, 
+      maximum: 100,
+      too_short: "должен содержать минимум %{count} символа",
+      too_long: "не может превышать %{count} символов"
+    }
+
+  #цена
+  validates :price, 
+    presence: { message: "не может быть пустым" },
+    numericality: { 
+      greater_than: 0, 
+      message: "должен быть больше 0" 
+    }
+
+  #киллограммы
+  validates :measurement_unit, 
+    presence: { message: "не может быть пустым" },
+    length: { 
+      minimum: 1, 
+      maximum: 10,
+      too_short: "должен содержать минимум %{count} символа",
+      too_long: "не может превышать %{count} символов"
+    }
+
+  #рейтинг
+  validates :rating, 
+    allow_nil: true,
+    numericality: { 
+      greater_than_or_equal_to: 0, 
+      less_than_or_equal_to: 5,
+      message: "должен быть от 0 до 5" 
+    }
 end
