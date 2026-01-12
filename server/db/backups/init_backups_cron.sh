58b10e5ea9d09c930c8aaeb617086b46b26437cc
Michael-Dolgov <michaeldlgov@gmail.com>
2026-01-12 22:59:59 +0300

    backups

---
+++ server/db/backups/init_backups_cron.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Путь к Rails-проекту
+RAILS_ROOT="/Users/michael/Documents/grocery-shop/server"
+
+# Папка для логов
+LOG_DIR="$RAILS_ROOT/log"
+
+# Файл лога
+LOG_FILE="$LOG_DIR/backup.log"
+
+# Переходим в корень проекта
+cd "$RAILS_ROOT" || exit
+
+/bin/bash -c "bin/rails db:json_backup >> \"$LOG_FILE\" 2>&1"
