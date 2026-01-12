4af499d13b5f28215ad3365a61b15644234024b2
SilerHofe <sashsa220605@mail.ru>
2026-01-12 20:01:55 +0300

    Добавил норм валидацию на order и order_item

--- server/app/models/order_item.rb
+++ server/app/models/order_item.rb
@@ -2,6 +2,27 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
-  validates :quantity, numericality: { greater_than: 0 }
-  validates :price_at_order, numericality: { greater_than: 0 }
+  #количество
+  validates :quantity, 
+    presence: { message: "не может быть пустым" },
+    numericality: { 
+      greater_than: 0, 
+      message: "должен быть больше 0" 
+    }
+
+  #ID заказа
+  validates :order_id, 
+    presence: { message: "не может быть пустым" }
+
+  #ID продукта
+  validates :product_id, 
+    presence: { message: "не может быть пустым" }
+
+  #Цена при заказе
+  validates :price_at_order, 
+    presence: { message: "не может быть пустым" },
+    numericality: { 
+      greater_than: 0, 
+      message: "должен быть больше 0" 
+    }
 end
