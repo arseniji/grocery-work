df7eab3fe1476b2e8702db980466d3153d280b90
Schcerbakov Maxim <miko.r4r@gmail.com>
2026-01-15 18:47:14 +0300

    feat: add nav

--- client/src/widgets/admin-header/ui/styled.ts
+++ client/src/widgets/admin-header/ui/styled.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Header = styled.header`
+  border-radius: 0 0 20px 20px;
   background-color: #fcfcfc;
   box-shadow: 0px 5px 8px 0px rgba(34, 60, 80, 0.2);
   padding: 20px 40px;
--- client/src/widgets/admin-nav/ui/index.tsx
+++ client/src/widgets/admin-nav/ui/index.tsx
@@ -1,5 +1,15 @@
+import { Button } from "@/shared/ui";
 import { Nav } from "./styled";
 
 export const AdminNav = () => {
-  return <Nav></Nav>;
+  return (
+    <Nav>
+      <Button as="link" href="/admin/users">
+        Пользователи
+      </Button>
+      <Button as="link" href="/admin/products">
+        Продукты
+      </Button>
+    </Nav>
+  );
 };
