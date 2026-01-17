ca2f989f80e23132a85d5db0e801430a37676f7d
KotDev <kotikov.lil@bk.ru>
2026-01-17 16:53:02 +0300

    фикс уомманд

--- server/app/models/commands/order_commands.rb
+++ server/app/models/commands/order_commands.rb
@@ -1,6 +1,8 @@
-require_relative '../commands/order_commands' 
+require_relative 'base_command' unless defined?(BaseCommand)
+
 # Команда для обновления заказа
-class UpdateOrderCommand < BaseCommand
+unless defined?(UpdateOrderCommand)
+  class UpdateOrderCommand < BaseCommand
   def initialize(user_id:, order_id:, order_data:, previous_order_data:, current_user_id:)
     super(user_id: user_id, description: "Обновление заказа ##{order_id}")
     @order_id = order_id
@@ -16,10 +18,12 @@ class UpdateOrderCommand < BaseCommand
   def undo
     AdminOrderManager.update_orders(@previous_order_data, nil, @order_id, @current_user_id, use_command: false)
   end
+  end
 end
 
 # Команда для добавления продукта в заказ
-class AddProductToOrderCommand < BaseCommand
+unless defined?(AddProductToOrderCommand)
+  class AddProductToOrderCommand < BaseCommand
   def initialize(user_id:, product_id:, order_id:, user_id_param:, current_user_id:, quantity: 1)
     super(user_id: user_id, description: "Добавление продукта ##{product_id} в заказ ##{order_id}")
     @product_id = product_id
@@ -51,10 +55,12 @@ class AddProductToOrderCommand < BaseCommand
       use_command: false
     )
   end
+  end
 end
 
 # Команда для удаления продукта из заказа
-class RemoveProductFromOrderCommand < BaseCommand
+unless defined?(RemoveProductFromOrderCommand)
+  class RemoveProductFromOrderCommand < BaseCommand
   def initialize(user_id:, product_id:, order_id:, user_id_param:, current_user_id:, quantity_to_remove:, previous_order_item_data:)
     super(user_id: user_id, description: "Удаление продукта ##{product_id} из заказа ##{order_id}")
     @product_id = product_id
@@ -120,5 +126,6 @@ class RemoveProductFromOrderCommand < BaseCommand
       
       AdminOrderManager.fetch_updated_order(@user_id_param, @order_id)
     end
+  end
   end
 end
