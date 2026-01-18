f3e4a1f216580363a13633446e7d5b11b8947a76
KotDev <kotikov.lil@bk.ru>
2026-01-18 23:21:45 +0300

    деплой

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -1,6 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', 'http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:5173'
+    origins 'http://localhost:5173', 
+            'http://127.0.0.1:5173',
+            'http://195.19.64.155:5173',  # ДОБАВЬТЕ
+            'http://localhost:4000',
+            'http://195.19.64.155:4000'
     
     resource '*',
       headers: :any,
